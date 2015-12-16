ba5d0eba92909ffacb2326933e5e2f055d099d8f
Michael Fladischer <FladischerMichael@fladi.at>
2015-12-16 13:02:42 +0100

    Use Perl warnings in checks and remove local library path.

--- checks/check_phpfpm.pl
+++ checks/check_phpfpm.pl
@@ -19,7 +19,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 use strict;
-use lib "/usr/local/nagios/libexec/";
+use warnings;
 
 use URI;
 use LWP::UserAgent;
--- checks/check_typo3.pl
+++ checks/check_typo3.pl
@@ -21,7 +21,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 use strict;
-use lib "/usr/local/nagios/libexec/";
+use warnings;
 
 use version;
 use LWP::UserAgent;
